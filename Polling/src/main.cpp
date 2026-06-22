d7db03ee25f14924d99347fb3249d93a4dcb3868
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-06-22 14:27:04 -0300

    Test 1
    Testing ESP32 Wi-Fi and connections

--- Polling/src/main.cpp
+++ Polling/src/main.cpp
@@ -1,24 +1,41 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
-const char* ssid = "AGSinn_5G";
+const char* ssid = "AGSinn_2.4G";
 const char* password = "AGS@385hc";
+#define PINO_STATUS 2
+WebServer server(80);
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
-
-  Serial.println("Connecting to WiFi...");
+  pinMode(PINO_STATUS, OUTPUT);
+  digitalWrite(PINO_STATUS, LOW);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-    Serial.println("\n--- Conexão Estabelecida com Sucesso! ---");
-    Serial.print("O IP da tua placa na rede é: ");
-    Serial.println(WiFi.localIP());
+
+  for(int i = 0; i < 5; i++) {
+    digitalWrite(PINO_STATUS, HIGH);
+    delay(150);
+    digitalWrite(PINO_STATUS, LOW);
+    delay(150);
+  }
+
+  server.on("/ligar", []() {
+    digitalWrite(PINO_STATUS, HIGH);
+    server.send(200, "text/plain", "Hardware LIGADO via rede local!");
+  });
+
+  server.on("/desligar", []() {
+    digitalWrite(PINO_STATUS, LOW);
+    server.send(200, "text/plain", "Hardware DESLIGADO via rede local!");
+  });
+
+  server.begin();
 }
 
+
 void loop() {
-  
+  server.handleClient();
 }
