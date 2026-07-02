e20cbd6d8037062290a9b253e6c95d9b6a22866f
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-07-02 15:35:08 -0300

    Add initial project structure and configuration files for PlatformIO

---
+++ EspToTtl/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#define RX_PIN 20
+#define TX_PIN 21
+#define RS485_EN_PIN 8
+
+void setup(){
+  Serial.begin(115200);
+  Serial1.begin (115200, SERIAL_8N1, RX_PIN, TX_PIN);
+
+  pinMode(RS485_EN_PIN, OUTPUT);
+  digitalWrite(RS485_EN_PIN, HIGH);
+
+  delay(1000);
+  Serial.println("Iniciando transmissao via RS485...");
+}
+
+void loop(){
+  for (int i = 1; i < 5; i++){
+    Serial1.print("Número: ");
+    Serial1.println(i);
+
+    Serial.print("Enviando pro RS485: ");
+    Serial.println(i);
+
+    delay(1000);
+  }
+
+  Serial.println("--- Fim do ciclo. Reiniciando... ---");
+  delay(1000);
+}
