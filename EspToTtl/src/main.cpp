dd6e361bb973cbfc4f6501c0f620770dacf26bc1
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-07-03 11:27:57 -0300

    Refactor main.cpp to remove RS485_EN_PIN and update print statements for TTL communication

--- EspToTtl/src/main.cpp
+++ EspToTtl/src/main.cpp
@@ -2,17 +2,13 @@
 
 #define RX_PIN 44
 #define TX_PIN 43
-#define RS485_EN_PIN 1
 
 void setup(){
   Serial.begin(115200);
   Serial1.begin (115200, SERIAL_8N1, RX_PIN, TX_PIN);
 
-  pinMode(RS485_EN_PIN, OUTPUT);
-  digitalWrite(RS485_EN_PIN, HIGH);
-
   delay(1000);
-  Serial.println("Iniciando transmissao via RS485...");
+  Serial.println("Iniciando envio TTL puro para o PC2...");
 }
 
 void loop(){
@@ -20,7 +16,7 @@ void loop(){
     Serial1.print("Numero: ");
     Serial1.println(i);
 
-    Serial.print("Enviando pro RS485: ");
+    Serial.print("Enviando pro PC2: ");
     Serial.println(i);
 
     delay(1000);
