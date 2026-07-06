a3ec465d70e46fcbf77569cd1f49e447b7fc21c4
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-07-06 12:00:56 -0300

    Update initialization message and modify data transmission format in loop

--- EspToTtl/src/main.cpp
+++ EspToTtl/src/main.cpp
@@ -13,16 +13,21 @@ void setup(){
   digitalWrite(RS485_CONTROL_PIN, LOW);
 
   delay(1000);
-  Serial.println("Iniciando envio TTL puro para o PC2 (vers 1.0)...");
+  Serial.println("Iniciando envio TTL puro para o PC2 (vers 2.0 ITOA)...");
 }
 
 void loop(){
+
+  char buffer [3];
+
   for (int i = 0; i < 9; i++){
+
+    itoa(i, buffer, 10);
+
     digitalWrite(RS485_CONTROL_PIN, HIGH);
     delay(5);
 
-    //Serial1.print("");
-    Serial1.printf("%c", i);
+    Serial1.print(buffer);
 
     Serial1.flush();
     delay(5);
