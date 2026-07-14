d3f2c95f9a75c1970b0dda94d6f97ef3e5d78b2c
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-07-14 13:48:47 -0300

    Refactor main.cpp to improve variable naming and enhance readability in setup and loop functions

--- EspToTtl/src/main.cpp
+++ EspToTtl/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <esp_task_wdt.h>
 
-#define RX_PIN 1
-#define TX_PIN 2
+#define RX_PIN RX
+#define TX_PIN TX
 
 #define RS485_CONTROL_PIN 13
 
-int i = 0;
+int contador = 0;
 char buffer [4];
 
 void setup(){
@@ -17,34 +17,36 @@ void setup(){
   digitalWrite(RS485_CONTROL_PIN, LOW);
 
   delay(1000);
-  Serial.println("Iniciando envio TTL puro para o PC2 (vers 4.0 ITOA)...");
+  Serial.println("=== Sistema RS485 Iniciado com Sucesso ===");
 }
 
 void loop(){
 
-    itoa(i, buffer, 10);
+    itoa(contador, buffer, 10);
 
     digitalWrite(RS485_CONTROL_PIN, HIGH);
     delay(5);
 
+    Serial1.print("Dado ESP32: ");
     Serial1.println(buffer);
-    delay(5);
 
     Serial1.flush();
     delay(5);
     digitalWrite(RS485_CONTROL_PIN, LOW);
     
-    Serial.print("Enviando pro PC2: ");
-    Serial.println(i);
+    Serial.print ("Enviado para o barramento RS485: ");
+    Serial.println(contador);
 
     delay(1000);
 
-    i++;
-    if (i > 5) {
-      i = 0;
+    contador++;
+    if (contador > 5) {
+      contador = 0;
       yield();
     }
+    
     esp_task_wdt_reset();
-  //Serial.println("--- Fim do ciclo. Reiniciando... ---");
+
+  Serial.println("--- Fim do ciclo. Reiniciando... ---");
   delay(10);
 }
