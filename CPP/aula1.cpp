7759d171eab91abb3fda86a91f62d42cab4b23bf
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-06 09:52:31 -0300

    Fix formatting in aula1.cpp and add VSCode configuration files

--- CPP/aula1.cpp
+++ CPP/aula1.cpp
@@ -1,9 +1,12 @@
-#include <iostream>; //Comando basico de entrada e saida do c++
+#include <iostream> //Comando basico de entrada e saida do c++
 
 using namespace std;
 
 int main (){
 
+    cout << "hello world\n Let's go study cpp\n";
+
+    system("pause");
     return 0;
 } 
 //int retorna inteiro
