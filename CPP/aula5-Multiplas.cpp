837535fb781d377fd6f1c1476becafb472bb28a9
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-24 18:44:26 -0300

    Aula 5 - Multiplas Variaveis

---
+++ CPP/aula5-Multiplas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; 
+int main(){
+    /*
+    int vidas;
+    int tiros;
+    int life;
+
+    ou 
+
+    int vidas; int tiros; int life;
+
+    ou
+    */
+    int vidas=3, tiros=500, life=100; 
+
+    cout << vidas << "\n" << tiros << "\n" << life <<"\n";
+    
+    return 0;
+}
