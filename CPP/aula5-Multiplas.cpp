967fadfbd889342c467d1d06820ad9f96ef69c1d
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-24 18:50:49 -0300

    Escopo global #define

--- CPP/aula5-Multiplas.cpp
+++ CPP/aula5-Multiplas.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std; 
+
+#define pi 3.1415
+#define nome cout << "Icaro Ferreira Franklin\n\n"
+
 int main(){
     /*
     int vidas;
@@ -14,7 +18,8 @@ int main(){
     */
     int vidas=3, tiros=500, life=100; 
 
-    cout << vidas << "\n" << tiros << "\n" << life <<"\n";
+    nome;
+    cout << vidas << "\n" << tiros << "\n" << life <<"\n" << pi << "\n";
     
     return 0;
 }
