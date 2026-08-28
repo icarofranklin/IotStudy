bfd9cb1f346090f9cb543e518f3cea8503171dbc
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-28 16:09:19 -0300

    Aula 9 - If and Else

---
+++ CPP/aula9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // trabalhando com testes lógicos if / else
+    // int num1=10;
+    // int num2=20;
+    int sol=1;
+    //char opc='s';
+
+    if(sol){
+        cout<< "Vou ao clube\n";
+    }else{
+        cout<< "Vou ao cinema\n";
+    }
+
+    return 0;
+}
