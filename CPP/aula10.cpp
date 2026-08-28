decb58836ed538d66e6b7e2339bddb6f3921a07b
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-28 16:42:23 -0300

    Aula 10

---
+++ CPP/aula10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n1,n2,res;
+    //char opc;
+
+    cout << "Digite o valor da nota 1: ";
+    cin >> n1;
+    cout << "Digite o valor da nota 2: ";
+    cin >> n2;
+
+    res=(n1+n2)/2;
+
+    if(res>=7){
+        cout << "\nO aluno foi aprovado!\n";
+    }else{
+        cout << "\nAluno reprovado!\n";
+    }
+
+    return 0;
+}
