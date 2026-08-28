0efea806880e1a2fbd89049208591574df319713
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-28 17:03:45 -0300

    Aula 10

--- CPP/aula10.cpp
+++ CPP/aula10.cpp
@@ -7,13 +7,26 @@ int main(){
 
     cout << "Digite o valor da nota 1: ";
     cin >> n1;
+
+    while (n1>10){   
+        cout << "Nota inválida! A nota máxima é 10.\n\n Digite a nota novamente: ";
+        cin >> n1;
+    }
+
     cout << "Digite o valor da nota 2: ";
     cin >> n2;
+
+    while (n2>10){   
+        cout << "Nota inválida! A nota máxima é 10.\n\n Digite a nota novamente: ";
+        cin >> n2;
+    }
 
     res=(n1+n2)/2;
 
     if(res>=7){
         cout << "\nO aluno foi aprovado!\n";
+    }else if(res>=5){
+        cout << "\nAluno de recuperação!\n";
     }else{
         cout << "\nAluno reprovado!\n";
     }
