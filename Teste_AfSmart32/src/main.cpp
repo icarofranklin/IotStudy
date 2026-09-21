af14a9d6460a90dc5f347a3101c329022ed7d2b8
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-09-21 16:22:59 -0300

    Fix formatting and structure in main.cpp by adjusting loop function placement and ensuring proper inclusion of Wire library

--- Teste_AfSmart32/src/main.cpp
+++ Teste_AfSmart32/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup() {
+ Wire.begin(21, 22);
  Serial.begin(115200);
  Serial.println("Teste dos Reles da AF Smart ESP32 Rev2.1");
-
 }
-
-
 void loop() {
-  
+
+
 }
