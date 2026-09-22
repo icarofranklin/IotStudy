a93d05472ac3f9f8896130c2cce797b60e85eeb9
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-09-22 15:40:54 -0300

    Reorganize setup function and update comments for device address clarification in main.cpp

--- Teste_AfSmart32/src/main.cpp
+++ Teste_AfSmart32/src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #include <Wire.h>
+/*
+Nesta placa, o PCF8574 responde em 0x27.
 
-void setup() {
- Wire.begin(21, 22);
- Serial.begin(115200);
- Serial.println("Teste dos Reles da AF Smart ESP32 Rev2.1");
+Se A2=A1=A0=0 → endereço 0x20
+
+Se A2=A1=A0=1 → endereço 0x27
+*/
+
+void setup() {  
+    Serial.begin(115200);
+    delay(500);
+    Wire.begin(21, 22);
+    Serial.println("Teste dos Reles da AF Smart ESP32 Rev2.1");
 }
 void loop() {
     for (byte endereco = 1; endereco < 127; endereco++) {
     Wire.beginTransmission(endereco);
     byte resposta = Wire.endTransmission();
 
-    if (resposta == 0) {
-        Serial.print("Encontrei dispositivo em 0x");
-        Serial.println(endereco, HEX);
+        if (resposta == 0) {
+            Serial.print("Encontrei dispositivo em 0x");
+            Serial.println(endereco, HEX);
+        }
     }
- }
+    Serial.println("Scan completo.");
+    delay(5000);
 }
