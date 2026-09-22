6f7325b643bbf80373104922b1444cdd80aa4db7
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-09-22 15:14:33 -0300

    Update VSCode extension recommendations and refine main.cpp loop structure for device detection

--- Teste_AfSmart32/src/main.cpp
+++ Teste_AfSmart32/src/main.cpp
@@ -7,6 +7,13 @@ void setup() {
  Serial.println("Teste dos Reles da AF Smart ESP32 Rev2.1");
 }
 void loop() {
+    for (byte endereco = 1; endereco < 127; endereco++) {
+    Wire.beginTransmission(endereco);
+    byte resposta = Wire.endTransmission();
 
-
+    if (resposta == 0) {
+        Serial.print("Encontrei dispositivo em 0x");
+        Serial.println(endereco, HEX);
+    }
+ }
 }
